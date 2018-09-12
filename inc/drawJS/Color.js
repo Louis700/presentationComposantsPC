5f8e14e493f0063e486add15d5000bcd789828b9
Louis <louis.masson@palissy.fr>
2018-09-12 16:06:14 +0200

    Add index.hmtml and add glabal style

--- inc/drawJS/Color.js
+++ inc/drawJS/Color.js
@@ -1,14 +1,43 @@
 "use strict";
 
 class Color {
-	constructor(r, g=r, b=g, a=1) {
-		this.r = r;
-		this.g = g;
-		this.b = b;
-		this.a = a;
+	constructor(a, b=a, c=b, d=1, type=ColorType.RGB) {
+		this.type = type;
+
+		if(this.type === ColorType.RGB) {
+			this.r = a;
+			this.g = b;
+			this.b = c;
+			this.a = d;
+		} else if(this.type === ColorType.HSL) {
+			this.h = a;
+			this.s = b;
+			this.l = c;
+			this.a = d;
+		} else 
+			return ("The type isn't defined !" , new Color(255));
 	}
 
 	toString() {
-		return "rgba(" + this.r + "," + this.g + "," + this.b + ", " + this.a + ")";
+		if(this.type === ColorType.RGB)
+			return "rgba(" + this.r + "," + this.g + "," + this.b + ", " + this.a + ")";
+		else if(this.type === ColorType.HSL)
+			return "hsla(" + this.h + "," + this.s + "%," + this.l + "%," + this.a + ")";
+	}
+
+	static rgb(r, g=r, b=g) {
+		return Color.rgba(r, g, b);
+	}
+
+	static hsl(h, s=100, l=100) {
+		return Color.hsla(h, s, l, 1);
+	}
+
+	static rgba(r, g=r, b=g, a=1) {
+		return new Color(r, g, b, a);
+	}
+
+	static hsla(h, s=100, l=100, a=1) {
+		return new Color(h, s, l, a, ColorType.HSL);
 	}
 }
